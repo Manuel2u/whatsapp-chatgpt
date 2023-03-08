672e929b3833a48010112e0ed56b0aa1fb743b10
Manuel2u <edodoo011@st.ug.edu.gh>
2023-03-08 14:53:46 +0000

    .

--- dist/config/whatsapp.config.js
+++ dist/config/whatsapp.config.js
@@ -1,5 +1,8 @@
 import { Client, LocalAuth, } from "whatsapp-web.js";
 const client = new Client({
-    authStrategy: new LocalAuth()
+    authStrategy: new LocalAuth(),
+    puppeteer: {
+        args: ['--no-sandbox', '--disable-setuid-sandbox'],
+    }
 });
 export default client;
--- src/config/whatsapp.config.ts
+++ src/config/whatsapp.config.ts
@@ -1,7 +1,10 @@
 import { Client, LocalAuth,} from "whatsapp-web.js"
 
 const client = new Client({
-    authStrategy : new LocalAuth()
+    authStrategy : new LocalAuth(),
+    puppeteer: {
+      args: ['--no-sandbox','--disable-setuid-sandbox'],
+    }
 })
 
 

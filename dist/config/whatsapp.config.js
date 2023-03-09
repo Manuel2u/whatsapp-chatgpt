7251de91d87c97048501c1a06fb639258ac5b722
Manuel2u <edodoo011@st.ug.edu.gh>
2023-03-09 01:18:08 +0000

    testing

--- dist/config/whatsapp.config.js
+++ dist/config/whatsapp.config.js
@@ -1,5 +1,19 @@
-import { Client, LocalAuth } from "whatsapp-web.js";
-const client = new Client({
-    authStrategy: new LocalAuth(),
+import { Client, RemoteAuth } from "whatsapp-web.js";
+import dotenv from "dotenv";
+dotenv.config();
+// Require database
+import { MongoStore } from "wwebjs-mongo";
+import mongoose from "mongoose";
+let client;
+// Load the session data
+mongoose.connect(process.env.MONGODB_URI || "").then(() => {
+    const store = new MongoStore({ mongoose: mongoose });
+    client = new Client({
+        authStrategy: new RemoteAuth({
+            store: store,
+            backupSyncIntervalMs: 300000,
+        }),
+    });
+    client.initialize();
 });
 export default client;
